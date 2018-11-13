e81c6fe6634ead34adb52cef3dc6a848fd0d55bf
suiyoubi <mike.tang96@gmail.com>
2018-11-12 18:15:46 -0800

    be able to update the basic info, add new rooms, need to integrate with the address endpoint later

--- app/hotel_management/hotel_management.js
+++ app/hotel_management/hotel_management.js
@@ -11,7 +11,7 @@ angular.module('myApp.hotelManagement', [
       controller: 'hotelManagementController'
     });
   }])
-  .controller('hotelManagementController', function ($scope, $http, $rootScope) {
+  .controller('hotelManagementController', function ($scope, $http, $rootScope, $mdDialog) {
 
     $scope.searchHotel = function () {
 
@@ -39,6 +39,49 @@ angular.module('myApp.hotelManagement', [
     $scope.manageHotel = function (hotel) {
       console.error(hotel);
       $scope.selectedHotel = hotel;
+
+      //get all rooms for the hotel
+
+      const getRoomsUrl = `${$rootScope.url}/hotels/${hotel.id}/room-types`;
+
+      console.log(getRoomsUrl);
+
+      $http.get(getRoomsUrl).then(function (res) {
+        $scope.roomTypes = res.data;
+      }, function (err) {
+        console.error(err);
+      })
     };
 
+    $scope.addRoom = function() {
+      $mdDialog.show({
+        controller: 'roomTypeInputController',
+        templateUrl: 'dialog2.tmpl.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose:true,
+        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      }).then( function (roomType) {
+
+        const addRoomForHotelUrl = `${$rootScope.url}/hotels/${$scope.selectedHotel.id}/room-types`;
+        $http.post(addRoomForHotelUrl, roomType).then(function (res) {
+          roomType.id = res.data.id;
+          $scope.roomTypes.push(roomType);
+        }, function (err) {
+          console.error(err);
+        });
+
+      });
+    };
+
+    $scope.updateHotel = function () {
+
+      const updateHotelUrl = `${$rootScope.url}/hotels/update`;
+      $http.put(updateHotelUrl, $scope.selectedHotel).then(function (res) {
+        console.log(res);
+        $rootScope.popUp('You have updated the info!', 'success', 'great');
+      }, function (err) {
+        console.error(err);
+        $rootScope.popUp('update failed! please check your input');
+      });
+    }
   });
