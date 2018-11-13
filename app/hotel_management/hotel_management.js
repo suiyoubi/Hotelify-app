4219d9f3e025a12b4492b5716f6f3206e806c90c
suiyoubi <mike.tang96@gmail.com>
2018-11-12 22:25:15 -0800

    revert the management page

--- app/hotel_management/hotel_management.js
+++ app/hotel_management/hotel_management.js
@@ -5,32 +5,32 @@ angular.module('myApp.hotelManagement', [
   'ngMessages',
   'ngTable',
   'ngRoute'])
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/hotel_management', {
-    templateUrl: 'hotel_management/hotel_management.html',
-    controller: 'hotelManagementController'
-  });
-}])
-.controller('hotelManagementController', function ($scope, $http, $rootScope) {
-
-  $scope.searchHotel = function () {
-
-    // GET api/hotels/
-    const url = `${$rootScope.url}/hotels/search`;
-
-    console.log(url);
-    $http.get(url, {
-      params: $scope.searchObj
-    }).then(function (res) {
-
-      $scope.hotels = res.data;
-    }, function (err) {
-      console.err(err);
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/hotel_management', {
+      templateUrl: 'hotel_management/hotel_management.html',
+      controller: 'hotelManagementController'
     });
-  };
+  }])
+  .controller('hotelManagementController', function ($scope, $http, $rootScope) {
 
-  $scope.hotels = [
-    {brand_name:"ex", branch_name:"ex", street:"ex"},
-    {brand_name:"ex", branch_name:"ex", street:"ex"},
-  ];
-});
+    $scope.searchHotel = function () {
+
+      // GET api/hotels/
+      const url = `${$rootScope.url}/hotels/search`;
+
+      console.log(url);
+      $http.get(url, {
+        params: $scope.searchObj
+      }).then(function (res) {
+
+        $scope.hotels = res.data;
+      }, function (err) {
+        console.err(err);
+      });
+    };
+
+    $scope.hotels = [
+      {brand_name:"ex", branch_name:"ex", street:"ex"},
+      {brand_name:"ex", branch_name:"ex", street:"ex"},
+    ];
+  });
