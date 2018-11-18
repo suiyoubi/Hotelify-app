becf5be8187209b01530f2eedd1b6173df492582
suiyoubi <mike.tang96@gmail.com>
2018-11-17 16:00:25 -0800

    add the statistics page

--- app/statistics/statistics.js
+++ app/statistics/statistics.js
@@ -12,4 +12,99 @@ angular.module('myApp.statistics', [
   }])
   .controller('statisticsController', function ($scope, $http, $rootScope, $location) {
 
+    $scope.yesOrNo = function(number) {
+      if(number) return 'Yes';
+      return 'No';
+    }
+
+    $http.get(`${$rootScope.url}/hotels`).then(function (res) {
+      $scope.hotels = res.data;
+    });//
+
+    $scope.hotelIdMap = {};
+
+    $http.get($rootScope.url + '/stats/user-count').then(function (res) {
+      $scope.user_count = res.data.result;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/hotel-count').then(function (res) {
+      $scope.hotel_count = res.data.result;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/reservation-count').then(function (res) {
+      $scope.reservation_count = res.data.result;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/hotel-room-count').then(function (res) {
+      $scope.number_rooms_detail = res.data;
+
+      res.data.forEach(hotel => {
+        if(!$scope.hotelIdMap[hotel.id]) {
+          $scope.hotelIdMap[hotel.id] = {};
+          $scope.hotelIdMap[hotel.id].roomCount = hotel.result;
+        } else{
+          $scope.hotelIdMap[hotel.id].roomCount = hotel.result;
+        }
+      });
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/max-room-reserved').then(function (res) {
+      $scope.max_room_per_reservation = res.data.numOfRoomsReserved;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/min-room-reserved').then(function (res) {
+      $scope.min_room_per_reservation = res.data.numOfRoomsReserved;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/most-popular-tag').then(function (res) {
+      $scope.most_popular_tag = res.data;
+      $scope.tags = [res.data];
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/revenue').then(function (res) {
+      $scope.total_revenue = res.data.result;
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/hotels-booked-by-all-users').then(function (res) {
+      res.data.forEach(hotel => {
+        if(!$scope.hotelIdMap[hotel.id]) {
+          $scope.hotelIdMap[hotel.id] = {};
+          $scope.hotelIdMap[hotel.id].bookedByAllUsers = true;
+        } else{
+          $scope.hotelIdMap[hotel.id].bookedByAllUsers = true;
+        }
+      });
+    }, function (err) {
+      console.error(err);
+    });//
+
+    $http.get($rootScope.url + '/stats/reservations-per-hotel').then(function (res) {
+      $scope.reservation_per_hotel = res.data;
+      res.data.forEach(hotel => {
+        if(!$scope.hotelIdMap[hotel.id]) {
+          $scope.hotelIdMap[hotel.id] = {};
+          $scope.hotelIdMap[hotel.id].reservationCount = hotel.result;
+        } else{
+          $scope.hotelIdMap[hotel.id].reservationCount = hotel.result;
+        }
+      });
+    }, function (err) {
+      console.error(err);
+    });
   });
